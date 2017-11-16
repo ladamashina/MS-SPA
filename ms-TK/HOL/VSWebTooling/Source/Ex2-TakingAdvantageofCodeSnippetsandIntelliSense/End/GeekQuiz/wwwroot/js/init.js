1e2fedaa36cd131ab6fc6e160a5232571ea7828f
maksim.sarvas <ladamashina@gmail.com>
2017-11-16 13:01:59 +0300

    removed all bin and obj folder

--- ms-TK/HOL/VSWebTooling/Source/Ex2-TakingAdvantageofCodeSnippetsandIntelliSense/End/GeekQuiz/wwwroot/js/init.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    $(document).ready(function () {
-        var audioElements = document.getElementsByTagName("audio");
-        var len = audioElements.length;
-        for (var i = 0; i < len; i++) {
-            audioElements[i].play();
-        }
-    });
-}());
